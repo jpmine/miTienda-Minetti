1d52169edbd3baf3a5c43e4cb7f11225107fe386
jpmine <jpmine@protonmail.com>
2022-05-17 13:07:03 -0300

    Correcciones pedidas por el tutor

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import ItemGuitar from "../ItemGuitar/ItemGuitar"
 
-export default function ItemDetail() {
-  //const [loading, setLoading] = useState(false)
-  const [guitars, setGuitars] = React.useState([])
-
-    React.useEffect(() => {
-        fetch('https://api.mercadolibre.com/sites/MLA/search?q=guitarras')
-        .then((res) => (res.json()))
-        .then((data) => setGuitars(data.results))
-        //.then((data) => console.log(data.results))
-        .catch((err) => console.error(err))
-    },[])
-    
+export default function ItemDetail({guitars}) {
   return (
+    <>
+      <h3>Listado de guitarras</h3>
       <div className='d-flex justify-content-center flex-wrap'>
-          {guitars.map((guitar) => (
-          <ItemGuitar guitar={guitar} key={guitar.id}/>
-          )
-      )}
+          {guitars.map((guitar) => <ItemGuitar guitar={guitar} key={guitar.id}/> )}
       </div>
-    );
-  }
+    </>
+    )
+  } 
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,9 +2,19 @@ import React, { useEffect, useState } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 
 export default function ItemDetailContainer() {    
-    return (
-            <div>
-                <ItemDetail />                
-            </div>
-        );
+    const [guitars, setGuitars] = React.useState([])
+
+    React.useEffect(() => {
+        fetch('https://api.mercadolibre.com/sites/MLA/search?q=guitarras')
+        .then((res) => (res.json()))
+        .then((data) => setGuitars(data.results))
+        //.then((data) => console.log(data.results))
+        .catch((err) => console.error(err))
+    },[])
+    
+  return (
+      <div>
+          <ItemDetail guitars={guitars} key={guitars.id}/>      
+      </div>
+    );
   }
