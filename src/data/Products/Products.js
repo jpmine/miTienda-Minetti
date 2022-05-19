d0689fb9427f57f5a1f5d7f3d83fb276c17116d0
jpmine <jpmine@protonmail.com>
2022-05-19 19:13:36 -0300

    Corrección en error de imagenes

--- src/data/Products/Products.js
+++ src/data/Products/Products.js
@@ -4,7 +4,7 @@ export const products = [
       title: "Fender Telecaster",
       stock: 10,
       price: 255000,
-      image: "img/fender-telecaster.jpg",
+      image: "/img/fender-telecaster.jpg",
       description: "Se caracteriza por la simplicidad en su diseño y por el sonido que se obtiene de sus dos pastillas de bobinado simple o single coil.",
       category: "electrica"
     },
@@ -13,7 +13,7 @@ export const products = [
       title: "Fender Standard Stratocaster",
       stock: 6,
       price: 228000,
-      image: "img/fender-strato.webp",
+      image: "/img/fender-strato.webp",
       description: "La Fender Stratocaster, popularmente conocida como Strat o Strato, es un modelo de guitarra eléctrica diseñado entre 1952 y 1954 por Leo Fender",
       category: "electrica"
     },
@@ -22,7 +22,7 @@ export const products = [
       title: "Gibson Les Paul 2018 Faded Worn Cherry",
       stock: 10,
       price: 232000,
-      image: "img/gibson-cherry.jpg",
+      image: "/img/gibson-cherry.jpg",
       description: "La guitarra Les Paul Faded personifica por completo el célebre toque y tono de Les Paul con un acabado enérgico y desgastado.",
       category: "acustica"
     },
@@ -31,7 +31,7 @@ export const products = [
       title: "Ibanez RG GIO GRX70QA de álamo sunburst",
       stock: 15,
       price: 65000,
-      image: "img/ibanez-rio.webp",
+      image: "/img/ibanez-rio.webp",
       description: "Si algo caracteriza a las guitarras de Ibanez es su estilo, con cuellos más delgados y diapasones más planos, electrónica de alta potencia y acabados coloridos.",
       category: "acustica"
     },
